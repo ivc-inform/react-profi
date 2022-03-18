46da1c90926668ceaf24c9a6283e91ef5db8d26c
Andrey Yudin <info@ivc-inform.ru>
2022-03-18 16:36:31 +0300

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,16 +15,17 @@ export const Navbar: FC<NavbarProps> = props => {
             <Row justify="end">
                 {
                     auth ?
-                        <Menu theme="dark" mode="horizontal" selectable={ false }>
+                        <>
                             <div style={ { color: "white" } }>Пользователь</div>
-                            <Menu.Item key={ 1 } onClick={ () => console.log("Выйти") }>Выйти</Menu.Item>
-                        </Menu>
+                            <Menu theme="dark" mode="horizontal" selectable={ false }>
+                                <Menu.Item key={ 1 } onClick={ () => console.log("Выйти") }>Выйти</Menu.Item>
+                            </Menu>
+                        </>
                         :
                         <Menu theme="dark" mode="horizontal" selectable={ false }>
                             <Menu.Item key={ 1 } onClick={ () => navigate(RouteName.LOGIN) }>Логин</Menu.Item>
                         </Menu>
                 }
-
             </Row>
         </Layout.Header>
     )
