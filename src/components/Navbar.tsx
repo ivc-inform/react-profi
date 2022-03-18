41709682325620ddecf88545e1b420e1eb7b2667
Andrey Yudin <info@ivc-inform.ru>
2022-03-18 16:31:31 +0300

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,30 @@
 import React, { FC } from "react";
-import { Layout, Row } from "antd";
+import { Layout, Menu, Row } from "antd";
+import { useNavigate } from "react-router-dom";
+import { RouteName } from "../router";
 
 export interface NavbarProps {
 }
 
 export const Navbar: FC<NavbarProps> = props => {
+    const navigate = useNavigate();
+    const auth = false
+
     return (
         <Layout.Header>
             <Row justify="end">
-                Пункт
+                {
+                    auth ?
+                        <Menu theme="dark" mode="horizontal" selectable={ false }>
+                            <div style={ { color: "white" } }>Пользователь</div>
+                            <Menu.Item key={ 1 } onClick={ () => console.log("Выйти") }>Выйти</Menu.Item>
+                        </Menu>
+                        :
+                        <Menu theme="dark" mode="horizontal" selectable={ false }>
+                            <Menu.Item key={ 1 } onClick={ () => navigate(RouteName.LOGIN) }>Логин</Menu.Item>
+                        </Menu>
+                }
+
             </Row>
         </Layout.Header>
     )
