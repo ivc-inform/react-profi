f016a599a83aac9c0968da68f335fa69054c502c
uandtrew <uandrew1965@gmail.com>
2022-03-19 09:06:39 +0300

    .

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,16 @@
 /** @format */
 
 import React, { FC } from "react";
+import { Row } from "antd";
 
 export interface LoginProps {}
 
 export const Login: FC<LoginProps> = props => {
-    return <div>LOGIN</div>;
+    return (
+        <div>
+            <Row justify="center" align="middle" className="h100">
+                sddsdad
+            </Row>
+        </div>
+    );
 };
