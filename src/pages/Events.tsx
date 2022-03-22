d6f0573706f51900a9b367d70e965d6f381c7ba1
Andrey Yudin <info@ivc-inform.ru>
2022-03-22 15:31:17 +0300

    .

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,21 +1,28 @@
 /** @format */
 
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Button, Layout, Modal, Row } from "antd";
 import { EventCalendar } from "../components/EventCalendar";
 import { EventForm } from "../components/EventForm";
+import { useActions } from "../hooks/useActions";
 
 export interface EventsProps {}
 
 export const Events: FC<EventsProps> = props => {
     const [visible, setVisible] = useState(false);
+    const { setGuests } = useActions()
+
+    useEffect(() => {
+
+    }, []);
+
     return (
         <Layout>
-            <EventCalendar events={[]} />
-            <Row justify={"center"}>
-                <Button onClick={() => setVisible(true)}>Добавить событие</Button>
+            <EventCalendar events={ [] } />
+            <Row justify={ "center" }>
+                <Button onClick={ () => setVisible(true) }>Добавить событие</Button>
             </Row>
-            <Modal title={"Новое событие"} visible={visible} footer={null} onCancel={() => setVisible(false)}>
+            <Modal title={ "Новое событие" } visible={ visible } footer={ null } onCancel={ () => setVisible(false) }>
                 <EventForm />
             </Modal>
         </Layout>
--- src/store/reducers/action-creators.ts
+++ src/store/reducers/action-creators.ts
@@ -1,7 +1,9 @@
 /** @format */
 
 import { AuthActionCreators } from "./auth/action-creators";
+import { EventActionCreators } from "./event/action-creators";
 
 export const allActionCreators = {
-    ...AuthActionCreators
+    ...AuthActionCreators,
+    ...EventActionCreators
 };
