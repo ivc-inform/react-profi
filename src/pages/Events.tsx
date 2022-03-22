5eb184962495912f7cccbcd797c26ca81741800b
Andrey Yudin <info@ivc-inform.ru>
2022-03-22 14:29:39 +0300

    .

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,9 +1,27 @@
 /** @format */
 
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+import { Button, Layout, Modal, Row } from "antd";
+import { EventCalendar } from "../components/EventCalendar";
 
 export interface EventsProps {}
 
 export const Events: FC<EventsProps> = props => {
-    return <div>EVENT PAGE</div>;
+    const [visible, setVisible] = useState(false);
+    return (
+        <Layout>
+            <EventCalendar events={ [] } />
+            <Row justify={ "center" }>
+                <Button>
+                    Добавить событие
+                </Button>
+            </Row>
+            <Modal
+                title={ "Новое событие" }
+                visible={ visible }
+            >
+
+            </Modal>
+        </Layout>
+    );
 };
