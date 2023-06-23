e3f3118d82e68843d238ca3eda81b6e71e7e6ca7
Andrey Yudin <yudin_ag@ursip.ru>
2023-06-23 19:46:01 +0300

    .

--- src/store/userReducer.ts
+++ src/store/userReducer.ts
@@ -1,25 +1,45 @@
-const FETCH_USERS = "FETCH_USERS"
-const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS"
-const FETCH_USERS_ERROR = "FETCH_USERS_ERROR"
 interface UserState {
     users: any[];
     loading: boolean;
     error: string | null;
 }
 
+enum UserActionType {
+    FETCH_USERS = "FETCH_USERS",
+    FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS",
+    FETCH_USERS_ERROR = "FETCH_USERS_ERROR",
+}
+
+interface FetchUserAction {
+    type: UserActionType.FETCH_USERS
+}
+
+interface FetchUserSuccessAction {
+    type: UserActionType.FETCH_USERS_SUCCESS
+    payload: any[]
+}
+
+interface FetchUserErrorAction {
+    type: UserActionType.FETCH_USERS_ERROR
+    payload: string
+}
+
+type UserAction = FetchUserAction | FetchUserSuccessAction | FetchUserErrorAction
+
+
 const initialState: UserState = {
     users: [],
     loading: false,
     error: null
 };
-export const userReducer = (state = initialState, action): UserState => {
+export const userReducer = (state = initialState, action: UserAction): UserState => {
     switch (action.type) {
-        case FETCH_USERS:
-            return { loading: true, error: null, users: [] };
-        case FETCH_USERS_SUCCESS:
-            return { loading: false, error: null, users: action.payload };
-        case FETCH_USERS_ERROR:
-            return { loading: false, error: action.payload, users: [] };
+        case UserActionType.FETCH_USERS:
+            return {loading: true, error: null, users: []};
+        case UserActionType.FETCH_USERS_SUCCESS:
+            return {loading: false, error: null, users: action.payload};
+        case UserActionType.FETCH_USERS_ERROR:
+            return {loading: false, error: action.payload, users: []};
         default:
             return state;
     }
