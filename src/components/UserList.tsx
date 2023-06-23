0bded51fef6eb7b3eccc7733a92c6351c24d8bc5
Andrey Yudin <yudin_ag@ursip.ru>
2023-06-23 22:47:58 +0300

    .

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,8 +1,5 @@
 import React, { FC, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { UserState } from "../types/user";
 import { useTypedSelector } from "../hooks/useTypedSelector";
-import { fetchUsers } from "../store/action-creators/user";
 import { useAction } from "../hooks/useAction";
 
 export interface UserListProps {
@@ -10,7 +7,7 @@ export interface UserListProps {
 
 export const UserList: FC<UserListProps> = props => {
     const { users, error, loading } = useTypedSelector(state => state.user)
-    const {fetchUsers} =  useAction()
+    const { fetchUsers } = useAction()
 
     useEffect(() => {
         fetchUsers()
