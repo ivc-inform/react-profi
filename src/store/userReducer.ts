6a4593e21e29ae44fb2ea3aed941c58f42be7c1f
Andrey Yudin <yudin_ag@ursip.ru>
2023-06-23 19:48:24 +0300

    .

--- src/store/userReducer.ts
+++ src/store/userReducer.ts
@@ -1,37 +1,4 @@
-interface UserState {
-    users: any[];
-    loading: boolean;
-    error: string | null;
-}
 
-enum UserActionType {
-    FETCH_USERS = "FETCH_USERS",
-    FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS",
-    FETCH_USERS_ERROR = "FETCH_USERS_ERROR",
-}
-
-interface FetchUserAction {
-    type: UserActionType.FETCH_USERS
-}
-
-interface FetchUserSuccessAction {
-    type: UserActionType.FETCH_USERS_SUCCESS
-    payload: any[]
-}
-
-interface FetchUserErrorAction {
-    type: UserActionType.FETCH_USERS_ERROR
-    payload: string
-}
-
-type UserAction = FetchUserAction | FetchUserSuccessAction | FetchUserErrorAction
-
-
-const initialState: UserState = {
-    users: [],
-    loading: false,
-    error: null
-};
 export const userReducer = (state = initialState, action: UserAction): UserState => {
     switch (action.type) {
         case UserActionType.FETCH_USERS:
