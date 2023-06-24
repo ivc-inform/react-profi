79e02f83628d71c79591f8a63444244acd5b9197
Andrey Yudin <yudin_ag@ursip.ru>
2023-06-24 03:38:17 +0300

    .

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -7,24 +7,33 @@ export interface TodoListProps {
 }
 
 export const TodoList: FC<TodoListProps> = props => {
-    const { page, error, limit, loading, todos } = useTypedSelector(state => state.todo)
-    const {fetchTodos} =  useAction()
+    const { page, error, limit, loading, todos } = useTypedSelector(state => state.todo);
+    const { fetchTodos } = useAction();
+    const pages = [1, 2, 3, 4, 5];
 
     useEffect(() => {
-        fetchTodos(page, limit)
+        fetchTodos(page, limit);
     }, []);
 
     if (loading) {
-        return <h1>Идет загрузка...</h1>
+        return <h1>Идет загрузка...</h1>;
     }
 
     if (error) {
-        return <h1>{ error }</h1>
+        return <h1>{error}</h1>;
     }
 
     return (
-        <>
-            { todos.map(todo => <div key={ todo.id }>{ todo.id }. { todo.title }</div>) }
-        </>
-    )
+        <div>
+            {todos.map(todo => <div key={todo.id}>{todo.id}. {todo.title}</div>)}
+            <div style={{display:'flex'}}>
+                {pages.map(p=>
+                    <div
+                        style={{border: p === page? '2px solid red': '1px solid teal', padding : 10, cursor:'pointer'}}
+                        key={p}>
+                        {p}
+                    </div>)}
+            </div>
+        </div>
+    );
 };
